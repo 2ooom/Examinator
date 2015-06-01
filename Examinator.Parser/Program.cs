4ec11d31b7775c69dab0dedb10782cf8413433c5
2ooom <dmitry.parfenchik@gmail.com>
2015-06-01 21:58:59 +0200

    giving up pdf parsing in favor of xml

--- Examinator.Parser/Program.cs
+++ Examinator.Parser/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Examinator.Parser
 {
@@ -10,6 +6,13 @@ namespace Examinator.Parser
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Arguments format is the following: [source] [output]");
+                return;
+            }
+            var sourcePdf = args[0];
+            var output = args[1];
         }
     }
 }
